d20481eec76fa53cbac7294385665c1e21ad57a0
amansrtn <aman18may18@gmail.com>
2024-04-16 13:40:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI,Request
+import logging
+import pickle
+import string
+from textblob import TextBlob
+from nltk.corpus import stopwords
+from pathlib import Path
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
+
+
+@app.post("/safety_checker")
+async def generate_quiz(request: Request):
+    try:
+        text = await request.json()
+        result_text = text["text"]
+        c = Predict_Dark_Pattern_Type(result_text)
+        return str(c)
+    except Exception as e:
+        logging.info(e)
+
+
+@app.get("/")
+async def api_info():
+    return "CodeSage"
+
+
+def Predict_Dark_Pattern_Type(new_text):
+    model = pickle.load(open(Path(__file__).parent.joinpath("model.pkl"), "rb"))
+
+    tfidf_vectorizer = pickle.load(
+        open(Path(__file__).parent.joinpath("tfidf_vectorizer.pkl"), "rb")
+    )
+
+    DarkPatternType = ["safe", "unsafe"]
+    exclude = string.punctuation
+
+    def rempun(text):
+        return text.translate(str.maketrans("", "", exclude))
+
+    def spellcorr(text):
+        return TextBlob(text).correct().string
+
+    def remstop(text):
+        newtext = []
+        for w in text.split():
+            if w in stopwords.words("english"):
+                newtext.append("")
+            else:
+                newtext.append(w)
+        x = newtext[:]
+        newtext.clear()
+        return " ".join(x)
+
+    new_text = new_text.lower()
+    new_text = rempun(new_text)
+    new_text = spellcorr(new_text)
+    new_text = remstop(new_text)
+
+    X_new_text_tfidf = tfidf_vectorizer.transform([new_text]).toarray()
+
+    predicted_value = model.predict(X_new_text_tfidf)
+    predicted_val = model.predict_proba(X_new_text_tfidf)
+    return predicted_value[0]
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=80)
---
+++ test.py
@@ -0,0 +1,20 @@
+import requests
+
+# Define the URL of your FastAPI endpoint
+url = "http://127.0.0.1:80/safety_checker"  # Replace with your actual URL
+
+# Define the input text
+text = "help help"
+
+# Send a POST request with the input text
+response = requests.post(
+    url, json={"text": text}, headers={"Content-Type": "application/json"}
+)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Print the output
+    print("Generated output:")
+    print(response.text)
+else:
+    print("Error:", response.status_code)
